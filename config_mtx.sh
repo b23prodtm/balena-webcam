882b139cab03ec990ba8d8ebf2ef2ea5b96648c6
BRKTT <b23prodtm@users.sourceforge.net>
2024-08-25 01:00:21 +0200

    shellcheck

--- config_mtx.sh
+++ config_mtx.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ $1 = "rpicamera" ]; then
+if [ "$1" = "rpicamera" ]; then
     cp -fv rtsp-simple-server-rpi.yml mediamtx.yml
 else
     cp -fv rtsp-simple-server.yml mediamtx.yml
