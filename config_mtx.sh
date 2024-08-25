1a34540081ff989c12f48fa03db07bb1875e4be4
BRKTT <b23prodtm@users.sourceforge.net>
2024-08-25 22:37:58 +0200

    config_mtx

--- config_mtx.sh
+++ config_mtx.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 if [ -n "$MEDIAMTX_FILE" ]; then
-    printf "[WARN]: Please set a value for %s !\n" "MEDIAMTX_FILE";
+    printf "[WARN]: Please set a value for %s !\n" "MEDIAMTX_FILE"
 else
     if [ "$1" = "rpicamera" ]; then
         cp -fv rtsp-simple-server-rpi.template "$MEDIAMTX_FILE"
     else    
         cp -fv rtsp-simple-server.template "$MEDIAMTX_FILE"
     fi
+fi
