eddcd4339a980acca615e550fefaa5d0944069cc
Tina B Rakoto <webmaster@b23prodtm.info>
2024-11-24 21:06:13 +0100

    config

--- config_mtx.sh
+++ config_mtx.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-if [ ! -n "$MEDIAMTX_FILE" ]; then
+if [ -z "$MEDIAMTX_FILE" ]; then
     printf "[WARN]: Please set a value for %s !\n" "MEDIAMTX_FILE"
 else
-    if [ ! -n "$MTX_PATH" ]; then
+    if [ -z "$MTX_PATH" ]; then
       export MTX_PATH=cam      
     fi
-    if [ ! -n "$1" ]; then      
+    if [ -z "$1" ]; then      
       printf "[$0]: Used %s as the default stream.\n" "$MTX_PATH"
     else
       export MTX_PATH="$1"
