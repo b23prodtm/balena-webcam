b0d9b7c4497096c169e4f95407df88c26ee62c90
Tina B Rakoto <webmaster@b23prodtm.info>
2024-11-24 20:18:55 +0100

    cam RPI default

--- config_mtx.sh
+++ config_mtx.sh
@@ -2,11 +2,13 @@
 if [ ! -v MEDIAMTX_FILE ]; then
     printf "[WARN]: Please set a value for %s !\n" "MEDIAMTX_FILE"
 else
+    if [ ! -v "$MTX_PATH" ]; then
+      export MTX_PATH=cam      
+    fi
     if [ ! -v "$1" ]; then      
       printf "[$0]: Used %s as the default stream.\n" "$MTX_PATH"
-    fi
-    if [ ! -v "$MTX_PATH" ]; then
-      export MTX_PATH=webcam
+    else
+      export MTX_PATH=$1
     fi
     cp -fv rtsp-simple-server-"$MTX_PATH".template "$MEDIAMTX_FILE"
 fi
