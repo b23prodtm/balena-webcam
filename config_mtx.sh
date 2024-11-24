6c335914d862364c3e01591a9c0831736dd0a956
TiB Rkt Arimana <5566338+b23prodtm@users.noreply.github.com>
2024-11-24 20:24:35 +0100

    Feature/issue 76 (#77)
    *
    
    * cam RPI default

--- config_mtx.sh
+++ config_mtx.sh
@@ -2,9 +2,13 @@
 if [ ! -v MEDIAMTX_FILE ]; then
     printf "[WARN]: Please set a value for %s !\n" "MEDIAMTX_FILE"
 else
-    if [ "$1" = "rpicamera" ]; then
-        cp -fv rtsp-simple-server-rpi.template "$MEDIAMTX_FILE"
-    else    
-        cp -fv rtsp-simple-server.template "$MEDIAMTX_FILE"
+    if [ ! -v "$MTX_PATH" ]; then
+      export MTX_PATH=cam      
     fi
+    if [ ! -v "$1" ]; then      
+      printf "[$0]: Used %s as the default stream.\n" "$MTX_PATH"
+    else
+      export MTX_PATH="$1"
+    fi
+    cp -fv rtsp-simple-server-"$MTX_PATH".template "$MEDIAMTX_FILE"
 fi
