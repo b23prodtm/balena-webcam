8f8ec8a4752d6e4eae970ffeb9a1658d70859e2c
Tina B Rakoto <webmaster@b23prodtm.info>
2024-11-24 19:32:06 +0100

    armhf pushed ./deployment/images/node-php7

--- config_mtx.sh
+++ config_mtx.sh
@@ -2,7 +2,7 @@
 if [ ! -v MEDIAMTX_FILE ]; then
     printf "[WARN]: Please set a value for %s !\n" "MEDIAMTX_FILE"
 else
-    if [ "$1" = "rpicamera" ]; then
+    if [ "$1" = "rpiCamera" ]; then
         cp -fv rtsp-simple-server-rpi.template "$MEDIAMTX_FILE"
     else    
         cp -fv rtsp-simple-server.template "$MEDIAMTX_FILE"
